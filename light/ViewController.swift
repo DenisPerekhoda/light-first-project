86c59006bc46a86bebd7f9f0b466f17a7eb98cd1
Denis Perekhoda <d.perekhoda@gmail.com>
2019-11-28 20:08:32 +0300

    Initial Commit

---
+++ light/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  light
+//
+//  Created by Denis Perekhoda on 28.11.2019.
+//  Copyright © 2019 Denis Perekhoda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
