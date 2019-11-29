f2e103e2c7058ea31edb17784a8866391159ee92
Denis Perekhoda <d.perekhoda@gmail.com>
2019-11-29 13:53:13 +0300

    Message

--- Light/ViewController.swift
+++ Light/ViewController.swift
@@ -6,28 +6,47 @@
 //  Copyright © 2019 Denis Perekhoda. All rights reserved.
 //
 
+import AVFoundation
 import UIKit
 
 class ViewController: UIViewController {
     
-    var lightOn = false
+    var isLightOn = true
 
     @IBAction func ButtonPressed(_ sender: UIButton) {
-            lightOn = !lightOn
-        updateUI()
+            isLightOn = !isLightOn
+        updateView()
     }
     
-    func updateUI() {
+    func updateView() {
         
-        view.backgroundColor = lightOn ? .white : .black
+        let device = AVCaptureDevice.default(for: AVMediaType.video)
+        
+        if let dev = device, dev.hasTorch {
+            view.backgroundColor = .black
+            do {
+                try dev.lockForConfiguration()
+                dev.torchMode = isLightOn ? .on : .off
+                dev.unlockForConfiguration()
+            } catch {
+                print(error)
+            }
+        } else {
+            view.backgroundColor = isLightOn ? .white : .black
+        }
+//        view.backgroundColor = lightOn ? .white : .black
         
 //        if lightOn {view.backgroundColor = .white }
 //        else { view.backgroundColor = .black}
     }
+    
+    override var prefersStatusBarHidden: Bool {
+        return true }
+        
     override func viewDidLoad() {
         super.viewDidLoad()
     
-        updateUI()
+        updateView()
     }
 }
 
